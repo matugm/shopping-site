34108e793e203d44b3a3901f53dddb4527d68351
matugm <matugm@gmail.com>
2015-03-08 19:12:06 +0100

    Make sure the product exist before adding it to the shopping cart

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,4 +1,14 @@
 class ShoppingCart < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validate :product_exist
+
+  def product_exist
+    begin
+      Product.find(product_id)
+    rescue ActiveRecord::RecordNotFound
+      errors.add(:product_id, "Product doesn't exist")
+    end
+  end
 end
