16da4f17326ae78bef27d6db177fb7ccc226a6de
matugm <matugm@gmail.com>
2015-03-08 05:26:00 +0100

    Add validation to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,15 @@ class CommentsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @product.comments.create(content: params[:content], product: @product)
-    redirect_to :root
+
+    if @product.save
+      flash[:notice] = "Comment has been added."
+      redirect_to :root
+    else
+      flash[:error] = @product.errors
+      redirect_to new_product_comment_path(@product.id)
+    end
+
   end
 
   def new
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :product
+
+  validates :content, length: { in: 10..200 }
 end
