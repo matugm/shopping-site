31d696a67f8cbacd7bb16e1ef4d7322e3ab43711
matugm <matugm@gmail.com>
2015-03-09 05:49:06 +0100

    Add bootstrap and basic styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 
   def shopping_cart_button(id)
     if current_user
-      button_to 'Add to shopping cart', shopping_cart_add_path(id), { method: 'post' }
+      button_to 'Add to shopping cart', shopping_cart_add_path(id), { method: 'post', class: 'btn btn-info' }
     end
   end
 end
